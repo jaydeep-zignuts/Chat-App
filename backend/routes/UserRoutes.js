e678d4766e26390c279bf01538a16bb1eac7c8a6
Jaydeep Patel <jaydeepp@zignuts.com>
2024-02-21 10:17:24 +0530

    remove database

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,6 +1,6 @@
 const route = require("express").Router();
 const UserController = require("../controller/UserController");
-route.post("/user/register", UserController.Register);
-route.post("/user/login", UserController.Login);
+// route.post("/user/register", UserController.Register);
+// route.post("/user/login", UserController.Login);
 route.get("/user", UserController.Ind);
 module.exports = route;
